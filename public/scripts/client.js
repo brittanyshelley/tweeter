ef1c1e6c918bd006aa809c661a3e64db4bbf8944
brittanyshelley <brittany.j.shelley@gmail.com>
2023-12-05 18:30:20 -0700

    fixed media display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,32 +7,32 @@
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
 
-// Function to render tweets
-const renderTweets = function (tweets) {
-  const $tweetsContainer = $('#tweets-container'); // jQuery selector
-  $tweetsContainer.empty(); // jQuery method to clear the container
+  // Function to render tweets
+  const renderTweets = function (tweets) {
+    const $tweetsContainer = $('#tweets-container'); // jQuery selector
+    $tweetsContainer.empty(); // jQuery method to clear the container
 
-  // Loop through tweets and append each tweet element to the container
+    // Loop through tweets and append each tweet element to the container
     tweets.forEach(tweet => {
-    const $tweet = createTweetElement(tweet); // the $tweet is a jQuery object returned from createTweetElement
-    $tweetsContainer.append($tweet); // jQuery method to append the tweet element
+      const $tweet = createTweetElement(tweet); // the $tweet is a jQuery object returned from createTweetElement
+      $tweetsContainer.append($tweet); // jQuery method to append the tweet element
 
-  });
-};
+    });
+  };
 
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
 
-// Function to create a tweet element
-  const createTweetElement = function(tweetData) {
+  // Function to create a tweet element
+  const createTweetElement = function (tweetData) {
     const $tweet = $(`
     <article class="tweet">
     <header class="tweet-header">
@@ -59,64 +59,62 @@ const escape = function (str) {
   };
 
   $("form").on("submit", function (event) {
-  event.preventDefault();
-  const formData = $("form").serialize();
-  const tweetContent = $('#tweet-text').val();
+    event.preventDefault();
+    const formData = $("form").serialize();
+    const tweetContent = $('#tweet-text').val();
 
-  //Error handling
-  if (tweetContent.length > 140) {
-    const errorMessage = $("#error-message");
-    errorMessage.text('Tweet too long');
-    $("#error-message").hide();
-    // Use slideDown to reveal the error message with a slow animation
-    $("#error-message").slideDown('slow', function () {
+    //Error handling
+    if (tweetContent.length > 140) {
+      const errorMessage = $("#error-message");
+      errorMessage.text('Tweet too long');
+      $("#error-message").hide();
+      // Use slideDown to reveal the error message with a slow animation
+      $("#error-message").slideDown('slow', function () {
         // After sliding down, fade out the error message after a delay
         $(this).delay(2000).slideUp('slow');
-    });
+      });
 
 
-  } else if (tweetContent === '' || tweetContent === null) {
-    const errorMessage = $("#error-message");
-    errorMessage.text('No tweet to display');
-    $("#error-message").hide();
-    // Use slideDown to reveal the error message with a slow animation
-    $("#error-message").slideDown('slow', function () {
+    } else if (tweetContent === '' || tweetContent === null) {
+      const errorMessage = $("#error-message");
+      errorMessage.text('No tweet to display');
+      $("#error-message").hide();
+      // Use slideDown to reveal the error message with a slow animation
+      $("#error-message").slideDown('slow', function () {
         // After sliding down, fade out the error message after a delay
         $(this).delay(2000).slideUp('slow');
-    });
+      });
 
-  } else {
-    //Posts the submitted tweet to "/tweets" page and then loads the tweets
-    $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: formData
-    })
-    .then(() => {
-      $('#tweet-text').val('');
-      $('.counter').val('140');
-      loadTweets();
-    })
-    .catch(function (err) {
-      console.log('Error: ', err);
-    });
-  }
-});
-//Grabs the post information and displays it on the tweeter page
-const loadTweets = function() {
-  $.ajax({
-    method: 'GET',
-    url: '/tweets',
-    success: function (tweetData) {
-      console.log('Success: ', tweetData);
-      renderTweets(tweetData);
-    },
-    error: function (err) {
-      console.log('Error: ', err);
+    } else {
+      //Posts the submitted tweet to "/tweets" page and then loads the tweets
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: formData
+      })
+        .then(() => {
+          $('#tweet-text').val('');
+          $('.counter').val('140');
+          loadTweets();
+        })
+        .catch(function (err) {
+          console.log('Error: ', err);
+        });
     }
   });
-
-};
-loadTweets();
-
+  //Grabs the post information and displays it on the tweeter page
+  const loadTweets = function () {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: function (tweetData) {
+        console.log('Success: ', tweetData);
+        renderTweets(tweetData);
+      },
+      error: function (err) {
+        console.log('Error: ', err);
+      }
+    });
+  };
+  loadTweets();
 });
